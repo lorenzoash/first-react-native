7b5f8dd92e5777e5d9731c207d670f8ad70ed29c
lorenzoash <z.lakyo@gmail.com>
2018-08-16 21:21:14 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state ={
+
+    }
+    this.state.customStyles = {
+      color: "red"
+    }
+    setInterval(() => {
+      if(this.state.customStyles.color == "red") {
+        this.setState({
+          customStyles: {
+            color: 'blue'
+          }
+        })
+      }else {
+          this.setState({
+            customStyles: {
+              color: 'red'
+            }
+          })
+        }
+
+    }, 800)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.welcome, this.state.customStyles]}> Prime Time League</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "blue"
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
